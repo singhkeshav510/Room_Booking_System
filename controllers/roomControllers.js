aaef1c48a3cacc03715a797b83c728736803c3fb
singhkeshav510 <keshavsingh200100@gmail.com>
2021-07-17 23:50:06 +0530

    Transfer from bitbucket

---
+++ controllers/roomControllers.js
@@ -0,0 +1,5 @@
+const allRooms = (req, res) => {
+  res.status(200).json({ success: true, message: "All Rooms" });
+};
+
+export { allRooms };
---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DB_LOCAL_URI:
+      "mongodb+srv://keshav:nitkboss@bookit.zz5yq.mongodb.net/book?retryWrites=true&w=majority",
+  },
+};
