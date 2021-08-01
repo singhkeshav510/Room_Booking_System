a81f064b754acd5f7962530507d5f3223c696c59
singhkeshav510 <keshavsingh200100@gmail.com>
2021-08-01 23:21:49 +0530

    Working on Project

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,5 +1,59 @@
-const allRooms = (req, res) => {
-  res.status(200).json({ success: true, message: "All Rooms" });
+import Room from "../models/room";
+
+// Get All rooms => /api/rooms
+const allRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json({ success: true, count: rooms.length, rooms });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
 };
 
-export { allRooms };
+// Create new room => /api/rooms
+const newRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body);
+    res.status(200).json({ success: true, room });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+// Get room details => /api/rooms/:id
+const getSingleRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Room not Found with this ID" });
+    }
+    res.status(200).json({ success: true, room });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+// Update room details => /api/rooms/:id
+const updateRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Room not Found with this ID" });
+    }
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+    res.status(200).json({ success: true, room });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+export { allRooms, newRoom, getSingleRoom };
